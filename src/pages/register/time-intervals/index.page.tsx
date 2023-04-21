c460c727cf203f1812b9b9758a543557f4ec56ad
artenlf <felipe.arten@gmail.com>
2023-04-21 08:29:00 -0300

    fix: start time - end time differential treatment and time format to minutes

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -1,3 +1,4 @@
+import { convertTimeStringToMinutes } from '@/utils/convert-time-string-to-minutes'
 import { getWeekDays } from '@/utils/get-week-days'
 import { zodResolver } from '@hookform/resolvers/zod'
 import {
@@ -35,10 +36,32 @@ const timeIntarvalsFormSchema = z.object({
     .transform((intervals) => intervals.filter((interval) => interval.enabled))
     .refine((intervals) => intervals.length > 0, {
       message: 'Você precisa selecionar pelo menos um dia da semana.',
-    }),
+    })
+    .transform((intervals) => {
+      return intervals.map((interval) => {
+        return {
+          weekDay: interval.weekDay,
+          startTimeInMinutes: convertTimeStringToMinutes(interval.startTime),
+          endTimeInMinutes: convertTimeStringToMinutes(interval.endTime),
+        }
+      })
+    })
+    .refine(
+      (intervals) => {
+        return intervals.every(
+          (interval) =>
+            interval.endTimeInMinutes - 60 >= interval.startTimeInMinutes,
+        )
+      },
+      {
+        message:
+          'O horário de término deve ser pelo menos 1 hora distante do horário de início.',
+      },
+    ),
 })
 
-type TimeIntervalsFormData = z.infer<typeof timeIntarvalsFormSchema>
+type TimeIntervalsFormInput = z.input<typeof timeIntarvalsFormSchema>
+type TimeIntervalsFormOutput = z.infer<typeof timeIntarvalsFormSchema>
 
 export default function TimeIntervals() {
   const {
@@ -47,7 +70,7 @@ export default function TimeIntervals() {
     control,
     watch,
     formState: { isSubmitting, errors },
-  } = useForm({
+  } = useForm<TimeIntervalsFormInput>({
     resolver: zodResolver(timeIntarvalsFormSchema),
     defaultValues: {
       intervals: [
@@ -71,8 +94,10 @@ export default function TimeIntervals() {
 
   const intervals = watch('intervals')
 
-  async function handleSetTimeIntervals(data: TimeIntervalsFormData) {
-    console.log(data)
+  async function handleSetTimeIntervals(data: any) {
+    const formData = data as TimeIntervalsFormOutput
+
+    console.log(formData)
   }
 
   return (
