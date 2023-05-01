c4d16898908499630c037c930dbb96a31cbfd501
artenlf <felipe.arten@gmail.com>
2023-05-01 09:13:06 -0300

    feat: add Calendar dates elements

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,7 @@
 import { globalStyles } from '@/styles/global'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import '../lib/dayjs'
 
 globalStyles()
 
