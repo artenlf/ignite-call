c11cdae240fc95121efb9cfd198386e9cb03c7aa
artenlf <felipe.arten@gmail.com>
2023-05-08 07:01:23 -0300

    feat: integrated Google API Calendar and Meet scheduling

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -1,10 +1,11 @@
-/* eslint-disable camelcase */
-import { prisma } from '@/lib/prisma'
 import dayjs from 'dayjs'
+import { google } from 'googleapis'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { z } from 'zod'
+import { getGoogleOAuthToken } from '../../../../lib/google'
+import { prisma } from '../../../../lib/prisma'
 
-export default async function handle(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
@@ -37,30 +38,59 @@ export default async function handle(
 
   if (schedulingDate.isBefore(new Date())) {
     return res.status(400).json({
-      messsage: 'Date is in the past.',
+      message: 'Date is in the past.',
     })
   }
 
-  const conflictScheduling = await prisma.scheduling.findFirst({
+  const conflictingScheduling = await prisma.scheduling.findFirst({
     where: {
       user_id: user.id,
       date: schedulingDate.toDate(),
     },
   })
 
-  if (conflictScheduling) {
+  if (conflictingScheduling) {
     return res.status(400).json({
-      messsage: 'There is another scheduling at the same time.',
+      message: 'There is another scheduling at at the same time.',
     })
   }
 
-  await prisma.scheduling.create({
+  const scheduling = await prisma.scheduling.create({
     data: {
       name,
       email,
       comments,
       date: schedulingDate.toDate(),
       user_id: user.id,
+    },
+  })
+
+  const calendar = google.calendar({
+    version: 'v3',
+    auth: await getGoogleOAuthToken(user.id),
+  })
+
+  await calendar.events.insert({
+    calendarId: 'primary',
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: comments,
+      start: {
+        dateTime: schedulingDate.format(),
+      },
+      end: {
+        dateTime: schedulingDate.add(1, 'hour').format(),
+      },
+      attendees: [{ email, displayName: name }],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+          },
+        },
+      },
     },
   })
 
